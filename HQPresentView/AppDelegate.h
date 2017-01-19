50702433d8fb61b900a01534f36deaca0a1f32c2
HuangQiang11 <huangqiang@huangqiangdemac-mini.local>
2017-01-19 14:36:44 +0800

    Initial Commit

---
+++ HQPresentView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HQPresentView
+//
+//  Created by huangqiang on 17/1/19.
+//  Copyright © 2017年 huangqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HQPresentView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HQPresentView
+//
+//  Created by huangqiang on 17/1/19.
+//  Copyright © 2017年 huangqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
